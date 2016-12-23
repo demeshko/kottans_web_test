fcedcbd913190900cb28f7697b4134e43f8708c3
Viacheslav Demeshko <demeshko@gmail.com>
2016-12-23 17:07:23 +0200

    Refactoring JS script

--- task1/trivia-quiz/js/script.js
+++ task1/trivia-quiz/js/script.js
@@ -1,38 +1,30 @@
 let totalQuestions = 0;
 let answerCounter = 0;
-function request() {
-		const xhr = new XMLHttpRequest();
-		xhr.open('GET', 'http://jservice.io/api/random', false);
-		xhr.send();
-		if (xhr.status != 200) {
-				console.log(xhr.status + ': ' + xhr.statusText); // пример вывода: 404: Not Found
-		} else {
-				const res = JSON.parse(xhr.responseText);
-				return res[0];
-		}
-}
-function compile() {
-	let response = request();
-	while (response.answer.split(" ").length > 1) {
-			response = request();
-	}
-	let answer = response.answer.replace(/<\/?[^>]+(>|$)/g, "");
-	console.log(response.answer.split(" ").length);
-	console.log(answer);
-	document.getElementsByClassName("question-id")[0].innerHTML = response.id;
-	document.getElementsByClassName("question-category")[0].innerHTML = response.category.title;
-	document.getElementsByClassName("question-description")[0].innerHTML = response.question;
-	document.getElementsByClassName("total-questions-counter")[0].innerHTML = totalQuestions;
-	// let template = Handlebars.compile($('#template').html());
-	// $('#updates').append(template(response));
+function getQuestion() {
+  $.get('http://jservice.io/api/random').done(function(data) {
+	  let question = data[0];
+    if (question.answer.split(" ").length > 1) {
+    	console.log("reject: " + question.answer);
+      getQuestion();
+    } else {
+    	console.log("accept: " + question.answer);
+      bindQuestion(question);
+    }
+  });
 }
 
-$(function() {
-  compile();
+function bindQuestion(question) {
+	let answer = question.answer.replace(/<\/?[^>]+(>|$)/g, "");
+	$(".question-id").text(question.id);
+	$(".question-category").text(question.category.title);
+	$(".question-description").text(question.question);
+	$(".total-questions-counter").text(totalQuestions);
+}
+
+$(function(){
+  getQuestion();
 	$("#skip").on("click", function() {
-			// app();
-			// $('#updates').empty();
-			totalQuestions++;
-			compile();
+  	totalQuestions++;
+	  getQuestion();
 	});
 });
