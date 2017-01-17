72e39b6e09ee6d0bda7652faf0192c7285af6988
Viacheslav Demeshko <demeshko@gmail.com>
2017-01-17 12:34:31 +0200

    Reindent trivia code

--- task1/trivia-quiz/js/script.js
+++ task1/trivia-quiz/js/script.js
@@ -2,17 +2,17 @@ let totalQuestions = 0;
 let answerCounter = 0;
 let questionAnswer = '';
 function getQuestion() {
-  $.get('https://jservice.io/api/random').done(function(data) {
-	  let question = data[0];
-    if (question.answer.split(" ").length > 1) {
-    	// console.log("reject: " + question.answer);
-      getQuestion();
-    } else {
-    	console.log("Answer: " + question.answer.replace(/<\/?[^>]+(>|$)/g, ""));
-      questionAnswer = question.answer.replace(/<\/?[^>]+(>|$)/g, "");
-      bindQuestion(question);
-    }
-  });
+    $.get('https://jservice.io/api/random').done(function(data) {
+     let question = data[0];
+     if (question.answer.split(" ").length > 1) {
+        	// console.log("reject: " + question.answer);
+            getQuestion();
+        } else {
+        	console.log("Answer: " + question.answer.replace(/<\/?[^>]+(>|$)/g, ""));
+            questionAnswer = question.answer.replace(/<\/?[^>]+(>|$)/g, "");
+            bindQuestion(question);
+        }
+    });
 }
 
 function shuffleArray(array) {
@@ -26,12 +26,11 @@ function shuffleArray(array) {
 }
 
 function addButtons(answer) {
-  let shuffledArray = shuffleArray(answer.split(""));
-  let len = shuffledArray.length;
+    let shuffledArray = shuffleArray(answer.split(""));
+    let len = shuffledArray.length;
     for (let i = 0; i < len; i++) {
-      // console.log(i);
-      let li = $('<li class="inline-block"><button class="character">' + shuffledArray[i] + "</button></li>");
-      $("#shuffled-answer").append(li);
+        let li = $('<li class="inline-block"><button class="character">' + shuffledArray[i] + "</button></li>");
+        $("#shuffled-answer").append(li);
     }
 }
 
@@ -41,46 +40,44 @@ function bindQuestion(question) {
 	$(".question-category").text(question.category.title);
 	$(".question-description").text(question.question);
 	$(".total-questions-counter").text(totalQuestions);
-  $(".correct-answers-counter").text(answerCounter);
-  addButtons(answer);
+    $(".correct-answers-counter").text(answerCounter);
+    addButtons(answer);
 }
 
-$(function(){
-  getQuestion();
-	$("#skip").on("click", function() {
-  	totalQuestions++;
-    $('#shuffled-answer').empty();
-    $('#answer-check').empty();
-	  getQuestion();
-	});
-
-  $("body").on("domChanged", function () {
-    let answer = '';
-    $( "#answer-check > li" ).each(function(index) {
-        answer += $(this).text();
+$(function() {
+    getQuestion();
+    $("#skip").on("click", function() {
+        totalQuestions++;
+        $('#shuffled-answer').empty();
+        $('#answer-check').empty();
+        getQuestion();
     });
-    if (answer ===  questionAnswer) {
-      $("#myModal").modal();
-      answerCounter++;
-      totalQuestions++;
-      $('#shuffled-answer').empty();
-      $('#answer-check').empty();
-      getQuestion();
-    }
-  });
 
-  $(document).on('click', '#shuffled-answer > li > button', function() {
-    let chr = $(this).parent('.inline-block');
-    $(this).parent('.inline-block').remove();
-    $('#answer-check').append(chr);
-    $("body").trigger("domChanged");
-  });
+    $("body").on("domChanged", function () {
+        let answer = '';
+        $( "#answer-check > li" ).each(function(index) {
+            answer += $(this).text();
+        });
+        if (answer ===  questionAnswer) {
+            $("#myModal").modal();
+            answerCounter++;
+            totalQuestions++;
+            $('#shuffled-answer').empty();
+            $('#answer-check').empty();
+            getQuestion();
+        }
+    });
 
-  $(document).on('click', '#answer-check > li > button', function() {
-    let chr = $(this).parent('.inline-block');
-    $(this).parent('.inline-block').remove();
-    $('#shuffled-answer').append(chr);
+    $(document).on('click', '#shuffled-answer > li > button', function() {
+        let chr = $(this).parent('.inline-block');
+        $(this).parent('.inline-block').remove();
+        $('#answer-check').append(chr);
+        $("body").trigger("domChanged");
+    });
 
-  });
-
+    $(document).on('click', '#answer-check > li > button', function() {
+        let chr = $(this).parent('.inline-block');
+        $(this).parent('.inline-block').remove();
+        $('#shuffled-answer').append(chr);
+    });
 });
